6970e7a5cf37c9ee9770c6c839b4dc865d357937
Nick Buonincontri <nbuonin@gmail.com>
2021-05-20 09:30:43 -0400

    gtag null check

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,16 @@ window.addEventListener('load', function() {
 });
 
 var getOutboundLink = function(url) {
-    gtag('event', 'click', {
-        'event_category': 'outbound',
-        'event_label': url,
-        'transport_type': 'beacon',
-        'event_callback': function(){window.open(url, '_blank');},
-        'event_timeout': 1500
-    });
+    if (typeof gtag !== 'undefined') {
+        gtag('event', 'click', {
+            'event_category': 'outbound',
+            'event_label': url,
+            'transport_type': 'beacon',
+            'event_callback': function(){window.open(url, '_blank');},
+            'event_timeout': 1500
+        });
+    } else {
+        window.open(url, '_blank');
+    }
+
 };
